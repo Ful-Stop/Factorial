efdb2f7c8db7a6bd7a881832f43b2b8f7f262c22
Patrick Hall <ptrnikz@gmail.com>
2021-11-17 11:32:29 -0500

    yea

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -13,25 +13,45 @@ public class Factorial {
 
     public static double calcE(){
         double E_New = 2;
-        double E_Last = 1;
+        double E = 1;
+        double E_Diff = E_New - E;
         int check = 1;
-        while(E_Last > .001){
+        while(E_Diff > .001){
             check ++;
-            E_Last = E_New;
-            E_New += 1/calcFactorial(check);
+            E = E_New;
+            E_New += 1.0/calcFactorial(check);
+            E_Diff = E_New - E;
         }
-        return E_Last;
+        return E;
+    }
+
+    public static double calcEX(int x){
+        double E = 1;
+        double E_New = E + x/calcFactorial(1);
+        double E_Diff = E_New - E;
+        int check = 1;
+        while(E_Diff > .001){
+            check ++;
+            E = E_New;
+            E_New += Math.pow(x, check)/calcFactorial(check);
+            E_Diff = E_New - E;
+        }
+        return E;
     }
 
     public static void main(String[] args) {
-	// calFactorial
+	// calcFactorial
         for (int i = 1; i <= 20; i ++)
             System.out.println(calcFactorial(i));
 
-
+    // calcE
+        System.out.println();
         System.out.println(calcE());
 
-
+    // calcEX
+        System.out.println();
+        for (int i = 1; i <= 10; i ++)
+            System.out.println(calcEX(i));
 
     }
 }
